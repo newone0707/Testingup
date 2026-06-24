7904d4c6cf69971a380dff51033789dd321bec26
newone0707 <yfree12324@gmail.com>
2026-06-24 22:43:35 +0530

    Update vars.py to use MONGO_URL

--- vars.py
+++ vars.py
@@ -10,14 +10,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "7335874062"))
 CREDIT = environ.get("CREDIT", "@adisxverified")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
-TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
-
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
-    AUTH_USERS.append(int(OWNER))
-  
+MONGO_URL = environ.get("MONGO_URL", "")
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
 
